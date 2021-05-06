a1310c7a100ac65afe3538478bcecf661403db27
Becca Steinbrecher <beccasteinbrecher@gmail.com>
2021-05-05 21:51:41 -0600

    Connect async data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
 import './App.scss'
 
-const App:React.StatelessComponent<{}> = () => (
-  <div className='container'>
-    <h1>Hello World, React!</h1>
-  </div>
-)
 
-export default App
+const App: React.SFC = () => {
+  const [shows, setShows] = useState('');
+
+  useEffect(() => {fetchAllShows()}, []);
+
+  const fetchAllShows = async (): Promise<any> => {
+    const response: any = await fetch('http://localhost:3000/shows');
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data)
+    } else {
+      //TODO: Error handle
+      throw new Error (response.statusText);
+    }
+  }
+
+  return (
+    <div className='container'>
+      <h1>Hello World, React!</h1>
+    </div>
+  )
+}
+
+export default App;
